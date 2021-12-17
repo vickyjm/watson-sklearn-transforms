9ada9ed06d40cc6960d532c2fc1e775a1c0cc2c3
María Victoria Jorge <mvjorgemauriello@gmail.com>
2021-12-17 00:05:29 -0400

    Update setup.py

--- setup.py
+++ setup.py
@@ -8,10 +8,7 @@ setup(
             This is a sample python package for encapsulating custom
             tranforms from scikit-learn into Watson Machine Learning
       ''',
-      url='https://github.com/vnderlev/sklearn_transforms/',
-      author='Vanderlei Munhoz',
-      author_email='vnderlev@protonmail.ch',
-      license='BSD',
+      url='https://github.com/vickyjm/sklearn_transforms/',
       packages=[
             'my_custom_sklearn_transforms'
       ],
